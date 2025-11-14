1334c4066027afc3f3a346d6f44623039754fbd8
otsuka <ryuta_otsuka@crosshead.co.jp>
2025-11-14 10:54:05 +0900

    - 脆弱性コード - CodeQlv3 -> v4

--- test.py
+++ test.py
@@ -2,5 +2,16 @@
 
 import os
 import sys
+import sqlite3
 
 # CWE で引っ掛かりそうなコードを記述
+
+def get_user(name):
+    conn = sqlite3.connect("example.db")
+    cur = conn.cursor()
+
+    # ❌ ユーザ入力を文字列連結 → SQLインジェクションの温床
+    query = f"SELECT * FROM users WHERE name = '{name}'"
+    cur.execute(query)
+
+    return cur.fetchall()
